ed3dcbbc6cc8d84c400013920f2341891cc41455
Borza Viktor <viktor.borza@jaydata.org>
2017-09-15 21:35:02 +0200

    update result handling

--- src/validation.ts
+++ src/validation.ts
@@ -34,18 +34,13 @@ export const sleep = timeout => new Promise(resolve => setTimeout(resolve, timeo
 export const waitToEvaluation = async (cid): Promise<any> => {
   let result: any = await getEvaluation(cid);
   logger.debug('waitToEvaluation', JSON.stringify(result, null, 4));
-  while (result.item && result.item.validationState && result.item.validationState.state === 'Inprogress...') {
+  while (result.item && result.item.validationState && result.item.validationState.state === 'PENDING') {
     await sleep(3000);
     result = await getEvaluation(cid);
     logger.debug('waitToEvaluation', JSON.stringify(result, null, 4));
   }
 
-  if (result && result.item && result.item.validationResult) {
-    logger.debug('waitToEvaluation result', JSON.stringify(result.item.validationResult, null, 4));
-    return result.item.validationResult;
-  }
-
-  return new Error('missing validation result');
+  return result
 };
 
 export const validate_module = async (name, version) => {
@@ -55,18 +50,12 @@ export const validate_module = async (name, version) => {
   var validationResult = await get(`/validation?${params.join('&')}`);
   logger.debug('validationResult', JSON.stringify(validationResult, null, 4));
 
-  return {
-    result: await waitToEvaluation(validationResult.cid),
-    cid: validationResult.cid
-  };
+  return await waitToEvaluation(validationResult.cid);
 };
 
 export const validate_package = async packageJSON => {
   var validationResult = await post(`/validation`, packageJSON);
   logger.debug('validationResult', JSON.stringify(validationResult, null, 4));
 
-  return {
-    result: await waitToEvaluation(validationResult.cid),
-    cid: validationResult.cid
-  };
+  return await waitToEvaluation(validationResult.cid);
 };
